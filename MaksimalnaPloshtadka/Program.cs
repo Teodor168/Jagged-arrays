e0d681b7d30a249aee8239cff3d117571387ee65
Teodor168 <teodor16iliev@gmail.com>
2024-05-14 23:08:32 +0300

    neshto

---
+++ MaksimalnaPloshtadka/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace MaksimalnaPloshtadka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            int[,] arrays = new int[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    arrays[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            int bestSum = int.MinValue;
+            int bestRow = 0;
+            int bestCol = 0;
+
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = 0; j < cols - 1; j++)
+                {
+                    int curSum = arrays[i, j] + 
+                        arrays[i, j + 1] +
+                        arrays[i + 1, j] + 
+                        arrays[i + 1, j + 1];
+
+                    if (curSum > bestSum)
+                    {
+                        bestSum = curSum;
+                        bestRow = i;
+                        bestCol = j;
+                    }
+                }
+            }
+            Console.WriteLine($"{arrays[bestRow, bestCol]} {arrays[bestRow, bestCol + 1]}");
+            Console.WriteLine($"{arrays[bestRow + 1, bestCol]} {arrays[bestRow + 1, bestCol + 1]}");
+        }
+    }
+}
---
+++ TriangleOfPasckal/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace TriangleOfPasckal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dai visochina: ");
+            int h = int.Parse(Console.ReadLine());
+
+            int[][] triangle = new int[h + 1][];
+
+            for (int row = 0; row < h; row++)
+            {
+
+                triangle[row] = new int[row + 1];
+
+            }
+
+            triangle[0][0] = 1;
+
+            for (int row = 0; row < h - 1; row++)
+            {
+
+                for (int col = 0; col <= row; col++)
+                {
+
+                    triangle[row + 1][col] += triangle[row][col];
+
+                    triangle[row + 1][col + 1] += triangle[row][col];
+
+                }
+
+            }
+
+            //print
+            int space = triangle.Length;
+            for (int row = 0; row < h; row++)
+            {
+                for (int i = 0; i <= space; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                space--;
+
+                for (int col = 0; col <= row; col++)
+                {
+                    Console.Write(triangle[row][col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
